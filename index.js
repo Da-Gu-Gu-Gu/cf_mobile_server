3d54cee0fe7b6273b288311f07a1acf9b6fafe07
hha <daguguguu@gmail.com>
2023-01-23 15:34:05 +0630

    add graphql

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hi')
+const dotenv=require('dotenv').config()
+const {AppolloServer}=require('apollo-server')
+const mongoose=require('mongoose')
+// console.log('hi')
+// SFeVQaAoeiaaOlLT
+
+console.log(process.env.MONGOSE_DB)
+
+const typeDefs=require('./graphql/typeDefs')
+const resolvers=require('./graphql/resolvers')
+
+const server=new AppolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(process.env.MONGOSE_DB,{
+    useNewUrlParser:true
+}).then(()=>{
+    console.log('success')
+    return server.listen({port:5000})
+}).then(res=>{
+    console.log(`runnion on ${res.url}`)
+}).catch((err)=>{
+    console.log(err)
+})
